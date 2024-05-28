e221f7bafa0ba2409598b0ab86f9d752a3d0565d
Matthew0314 <150466527+Matthew0314@users.noreply.github.com>
2024-05-27 21:56:49 -0400

    Added Enemy Stats

---
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats 
+{
+    private string unitName;
+    private string unitDescription;
+    private string unitClass;
+    private int level;
+
+    private int health;                 //base health
+    private int attack;                 //base attack
+    private int magic;                  //base magic
+    private int defense;                //base defense
+    private int resistance;             //base resistance to magic
+    private int speed;                  //base speed, determines if unit attacks first
+    private int evasion;                //base evasion, how often the unit dodges
+    private int luck;                   //base luck, increases chance of critical
+    private int movement;               //Movement
+
+    private bool airBorn;               //if unit is airborn, will be ablee to pass over impassble tiles and weak to bows/air magic
+    private bool mounted;               //if unit is mounted
+    private bool armored;               //if unit is armored, weak to magic and other armor breaking weapons
+    private bool whisper;               //Whisper type
+
+    private int healthBars;
+
+    public EnemyStats(string uName,string uDesc,string uType,int lev, int HP,int ATK,int MAG,int DEF,int RES,int SPD,int EVA,int LUCK,int MOVE,bool air,bool mount,bool arm,bool whisp, int hBars)
+    {
+        unitName = uName; 
+        unitDescription = uDesc; 
+        unitClass = uType; 
+        level = lev;
+        health = HP; 
+        attack = ATK; 
+        magic = MAG; 
+        defense = DEF;
+        resistance = RES;
+        speed = SPD; 
+        evasion = EVA;
+        luck = LUCK;
+        movement = MOVE; 
+        airBorn = air;
+        mounted = mount;
+        armored = arm;
+        whisper = whisp;   
+        healthBars = hBars;     
+    }
+
+     public string UnitName
+    {
+        get { return unitName; }
+        // set { unitName = value; }
+    }
+
+    public string UnitDesrciption
+    {
+        get { return unitDescription; }
+        // set { unitDescription = value; }
+    }
+
+    public string UnitClass
+    {
+        get { return unitClass; }
+        // set { unitClass = value; }
+    }
+
+    public int Level
+    {
+        get { return level; }
+        //set { level = value; }
+    }
+
+    public int Health
+    {
+        get { return health; }
+        //set { health = value; }
+    }
+
+    public int Attack
+    {
+        get { return attack; }
+        //set { attack = value; }
+    }
+
+    public int Magic
+    {
+        get { return magic; }
+        //set { magic = value; }
+    }
+
+    public int Defense
+    {
+        get { return defense; }
+        //set { defense = value; }
+    }
+
+    public int Resistance
+    {
+        get { return resistance; }
+        //set { resistance = value; }
+    }
+
+    public int Speed
+    {
+        get { return speed; }
+        //set { speed = value; }
+    }
+
+    public int Evasion
+    {
+        get { return evasion; }
+        //set { evasion = value; }
+    }
+
+    public int Luck
+    {
+        get { return luck; }
+        //set { luck = value; }
+    }
+
+    public int Movement
+    {
+        get { return movement; }
+        //set { movement = value; }
+    }
+
+    public bool AirBorn
+    {
+        get { return airBorn; }
+        //set { airBorn = value; }
+    }
+
+    public bool Mounted
+    {
+        get { return mounted; }
+        //set { mounted = value; }
+    }
+
+    public bool Armored
+    {
+        get { return armored; }
+        //set { armored = value; }
+    }
+
+    public bool Whisper
+    {
+        get { return whisper; }
+        //set { whisper = value; }
+    }
+
+    public int HealthBars
+    {
+        get { return healthBars; }
+        set { healthBars = value; }
+    }
+}
--- Assets/Scripts/MapManagers/Prologue/PrologueMap.cs
+++ Assets/Scripts/MapManagers/Prologue/PrologueMap.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Versioning;
@@ -17,6 +18,10 @@ public class PrologueMap : MonoBehaviour, IMaps
     private int length = 10;
     private int width = 10;
     private List<UnitStats> mapUnits;
+    private EnemyStats eStats;
+    public TextAsset enemyTextData;
+    private List<EnemyUnit> mapEnemies = new List<EnemyUnit>();
+    
     // [SerializeField] GameObject felix; //Remove Later
     // [SerializeField] GameObject lilith; //Remove Later
 
@@ -39,6 +44,57 @@ public class PrologueMap : MonoBehaviour, IMaps
         AddNewPlayers();
         unitRos.InitMapUnit(unitNum);
         PrintCharacters();
+        InitEnemies();
+    }
+
+    private void InitEnemies() {
+        
+        string[] data = enemyTextData.text.Split(new string[] { ",", "\n" }, StringSplitOptions.None);
+
+        for (int i = 23; i < data.Length - 1; i += 23)
+        {
+            string cName = data[i];
+            string cDesc = data[i + 1];
+            string cType = data[i + 2];
+            
+            int level = int.Parse(data[i + 3]);
+            int HP = int.Parse(data[i + 4]);
+            int ATK = int.Parse(data[i + 5]);  
+            int MAG = int.Parse(data[i + 6]);
+            int DEF = int.Parse(data[i + 7]);
+            int RES = int.Parse(data[i + 8]);
+            int SPD = int.Parse(data[i + 9]);   
+            int EVA = int.Parse(data[i + 10]);
+            int LUCK = int.Parse(data[i + 11]);
+            int MOVE = int.Parse(data[i + 12]);
+            
+            bool air = bool.Parse(data[i + 13]);
+            bool mount = bool.Parse(data[i + 14]);
+            bool armored = bool.Parse(data[i + 15]);
+            bool whisp = bool.Parse(data[i + 16]);
+            int healthBars = int.Parse(data[i + 17]);
+            
+
+            eStats = new EnemyStats(cName, cDesc, cType, level, HP, ATK, MAG, DEF, RES, SPD, EVA, LUCK, MOVE, air, mount, armored, whisp, healthBars);
+
+            string loadPrefab = data[i + 18];
+            int enemyX = int.Parse(data[i + 19]);
+            int enemyZ = int.Parse(data[i + 20]);
+            string AIenemy = data[i + 21];
+            
+            GameObject enemyPrefab = Resources.Load("Enemies/" + loadPrefab) as GameObject;
+            GameObject newEnemy = Instantiate(enemyPrefab, new Vector3(grid.grid[enemyX, enemyZ].GetXPos(), grid.grid[enemyX, enemyZ].GetYPos() + 0.005f, grid.grid[enemyX, enemyZ].GetZPos()), Quaternion.identity);
+            
+            Type type = Type.GetType(AIenemy);
+            IEnemyAI enemyAI = newEnemy.AddComponent(type) as IEnemyAI;
+            Debug.Log("Hello Enemies");
+            EnemyUnit enemy = newEnemy.GetComponent<EnemyUnit>();
+            enemy.stats = eStats;
+            Debug.Log("Hello Enemies");
+            mapEnemies.Add(enemy);
+
+            
+        }
     }
 
     public void AddNewPlayers()
